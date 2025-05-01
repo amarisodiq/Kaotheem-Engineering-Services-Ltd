222b0b2703cd9a05cb9dd685bf6ea957a4282a51
amarisodiq <amarisodiq@gmail.com>
2025-05-01 15:41:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css");
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+
+    navLinks.forEach(link => {
+      link.addEventListener('click', () => {
+        const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+          toggle: false
+        });
+        bsCollapse.hide();
+      });
+    });
+  });
+
+
+// loop
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollContainer = document.querySelector(".scrolling-content");
+
+  // Duplicate images to ensure seamless scrolling
+  scrollContainer.innerHTML += scrollContainer.innerHTML;
+});
